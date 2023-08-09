4de26340cb38de4cfcfcb987c3b891a0184e6e78
Pablo Ramiro Bustos Belzu <pablobb.ram@gmail.com>
2023-08-08 22:02:12 -0400

    Areglado icons menu

--- resources/views/layouts/menu-lateral.blade.php
+++ resources/views/layouts/menu-lateral.blade.php
@@ -22,83 +22,91 @@
             </a>
         </li> --}}
         <li class="sidenav-item active">
-            <a href="{{ route('home')}}" class="sidenav-link">
-                <i class="sidenav-icon feather icon-pie-chart"></i>
+            <a href="{{ route('home') }}" class="sidenav-link">
+                <i class="sidenav-icon feather icon-home"></i>
                 <div>Home</div>
             </a>
         </li>
         <li class="sidenav-item active">
-            <a href="{{ route('asistente.index')}}" class="sidenav-link">
-                <i class="sidenav-icon feather icon-pie-chart"></i>
+            <a href="{{ route('asistente.index') }}" class="sidenav-link">
+                <i class="sidenav-icon feather icon-user"></i>
                 <div>Asistente Virtual</div>
             </a>
         </li>
         @if (Auth::user()->tipo == 'admin')
-        <li class="sidenav-item active">
-            <a href="{{ route('planinternets.index')}}" class="sidenav-link">
-                <i class="sidenav-icon feather icon-pie-chart"></i>
-                <div>Planes de internet</div>
-            </a>
-        </li>
-        <li class="sidenav-item active">
-            <a href="{{ route('plan-tv-cables.index')}}" class="sidenav-link">
-                <i class="sidenav-icon feather icon-pie-chart"></i>
-                <div>Plan de tv cable</div>
-            </a>
-        </li>
-        <li class="sidenav-item active">
-            <a href="{{ route('plan-llamadas.index')}}" class="sidenav-link">
-                <i class="sidenav-icon feather icon-pie-chart"></i>
-                <div>Planes de llamadas</div>
-            </a>
-        </li>
-        <li class="sidenav-item active">
-            <a href="{{ route('tecnicos.index')}}" class="sidenav-link">
-                <i class="sidenav-icon feather icon-pie-chart"></i>
-                <div>Tecnicos</div>
-            </a>
-        </li>
-        <li class="sidenav-item active">
-            <a href="{{ route('tipo-servicios-tecnicos.index') }}" class="sidenav-link">
-                <i class="sidenav-icon feather icon-pie-chart"></i>
-                <div>Tipo de Servicio Tecnico</div>
-            </a>
-        </li>
-        <li class="sidenav-item active">
-            <a href="" class="sidenav-link">
-                <i class="sidenav-icon feather icon-pie-chart"></i>
-                <div>Combos promocionales</div>
-            </a>
-        </li>
-        <li class="sidenav-item">
-            <a href="javascript:" class="sidenav-link sidenav-toggle">
-                <i class="sidenav-icon feather icon-box"></i>
-                <div>Reportes</div>
-            </a>
-            <ul class="sidenav-menu">
-                <li class="sidenav-item active">
-                    <a href="{{route('procesar.formulario')}}" class="sidenav-link">
-                        <i class="sidenav-icon feather icon-pie-chart"></i>
-                        <div>Reporte Ordenes de trabajo</div>
-                    </a>
-                </li>
-                <li class="sidenav-item active">
-                    <a href="{{route('procesar.factura')}}" class="sidenav-link">
-                        <i class="sidenav-icon feather icon-pie-chart"></i>
-                        <div>Reporte Ingresos por Servicio</div>
-                    </a>
-                </li>
-                <li class="sidenav-item active">
-                    <a href="{{route('procesar.reporte')}}" class="sidenav-link">
-                        <i class="sidenav-icon feather icon-pie-chart"></i>
-                        <div>Reporte Interacciones por Año</div>
-                    </a>
-                </li>
-            </ul>
-        </li>
+            <li class="sidenav-item active">
+                <a href="{{ route('tecnicos.index') }}" class="sidenav-link">
+                    <i class="sidenav-icon feather icon-users"></i>
+                    <div>Tecnicos</div>
+                </a>
+            </li>
+            <li class="sidenav-item active">
+                <a href="{{ route('tipo-servicios-tecnicos.index') }}" class="sidenav-link">
+                    <i class="sidenav-icon feather icon-check"></i>
+                    <div>Tipo de Servicio Tecnico</div>
+                </a>
+            </li>
+            <li class="sidenav-item">
+                <a href="javascript:" class="sidenav-link sidenav-toggle">
+                    <i class="sidenav-icon feather icon-play"></i>
+                    <div>Servicios</div>
+                </a>
+                <ul class="sidenav-menu">
+                    <li class="sidenav-item active">
+                        <a href="{{ route('planinternets.index') }}" class="sidenav-link">
+                            <i class="sidenav-icon feather icon-monitor"></i>
+                            <div>Planes de internet</div>
+                        </a>
+                    </li>
+                    <li class="sidenav-item active">
+                        <a href="{{ route('plan-tv-cables.index') }}" class="sidenav-link">
+                            <i class="sidenav-icon feather icon-tv"></i>
+                            <div>Plan de tv cable</div>
+                        </a>
+                    </li>
+                    <li class="sidenav-item active">
+                        <a href="{{ route('plan-llamadas.index') }}" class="sidenav-link">
+                            <i class="sidenav-icon feather icon-phone"></i>
+                            <div>Planes de llamadas</div>
+                        </a>
+                    </li>
+                    <li class="sidenav-item active">
+                        <a href="" class="sidenav-link">
+                            <i class="sidenav-icon feather icon-shopping-cart"></i>
+                            <div>Combos promocionales</div>
+                        </a>
+                    </li>
+                </ul>
+            </li>
 
+            <li class="sidenav-item">
+                <a href="javascript:" class="sidenav-link sidenav-toggle">
+                    <i class="sidenav-icon feather icon-file-text"></i>
+                    <div>Reportes</div>
+                </a>
+                <ul class="sidenav-menu">
+                    <li class="sidenav-item active">
+                        <a href="{{ route('procesar.formulario') }}" class="sidenav-link">
+                            <i class="sidenav-icon feather icon-file"></i>
+                            <div>Reporte Ordenes de trabajo</div>
+                        </a>
+                    </li>
+                    <li class="sidenav-item active">
+                        <a href="{{ route('procesar.factura') }}" class="sidenav-link">
+                            <i class="sidenav-icon feather icon-file"></i>
+                            <div>Reporte Ingresos por Servicio</div>
+                        </a>
+                    </li>
+                    <li class="sidenav-item active">
+                        <a href="{{ route('procesar.reporte') }}" class="sidenav-link">
+                            <i class="sidenav-icon feather icon-file"></i>
+                            <div>Reporte Interacciones por Año</div>
+                        </a>
+                    </li>
+                </ul>
+            </li>
         @endif
-        
+
         {{-- <li class="sidenav-item active">
             <a href="" class="sidenav-link">
                 <i class="sidenav-icon feather icon-pie-chart"></i>
